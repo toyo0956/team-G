8e3ecdef8947cd1efb2cdd83dd8123289785ad20
toyo0956 <toyo.job56@gmail.com>
2019-12-06 15:26:01 +0900

    top-page-create

--- db/migrate/20191204091025_devise_create_users.rb
+++ db/migrate/20191204091025_devise_create_users.rb
@@ -6,7 +6,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname,           null: false
+      t.string :email,              null: false, default: ""
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false
+      t.string :birthdate_year,     null: false
+      t.string :birthdate_month,    null: false
+      t.string :birthdate_day,      null: false
+      t.string :phone_number,       null: false
+      t.string :encrypted_password, null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
