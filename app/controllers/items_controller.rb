419dd634ec73bf2f9ab09aa2349fec1575f7237a
michi-tech <michi.swd@gmail.com>
2019-12-13 12:13:33 +0900

    add items db create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+  
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
+  end 
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :condition, :feepayer, :method, :region, :category, :days, :price, :image)
   end
 end
