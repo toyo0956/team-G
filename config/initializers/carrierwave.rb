d0fdf93b19a32c457f7a02ef73c4d48646392a7c
NishiguchiHiroki <nishiguchihiroki2@gmail.com>
2019-12-13 23:44:31 +0900

    fog image upload test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,13 +7,14 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'mercari-team-g'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-team-g'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }else
-  config.storage :file
+  config.storage :fog
   config.enable_processing = false if Rails.env.test?
   end
 end
