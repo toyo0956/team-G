df59d3368f7323a2e6b0af0cdb418de9cb5881dd
fine-blue <57351103+fine-blue@users.noreply.github.com>
2019-12-20 19:41:24 +0900

    Update purchase_controller.rb

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -22,7 +22,6 @@ class PurchaseController < ApplicationController
 
 private
   def set_card
-    # @card = Card.where(user_id: current_user).first
     @card = Card.find(current_user.id)
   end
 end
