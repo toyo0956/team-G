55e85623de2bddde7d0c87b3ec3ef3110c1fd57c
fine-blue <57351103+fine-blue@users.noreply.github.com>
2019-12-20 20:42:43 +0900

    Update purchase_controller.rb

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,15 +1,13 @@
 class PurchaseController < ApplicationController
-  before_action :set_card
+  before_action :set_card, :set_item
 
   def show
-    @item = Item.find(params[:id])
     Payjp.api_key = ENV["PAYJP_TEST_SECRET_KEY"]
     customer = Payjp::Customer.retrieve(@card.customer_id)
     @default_card_information = customer.cards.retrieve(@card.card_id)
   end
 
   def pay
-    @item = Item.find(params[:id])
     Payjp.api_key = ENV["PAYJP_TEST_SECRET_KEY"]
     Payjp::Charge.create(
       :amount => @item.price,
@@ -22,5 +20,9 @@ class PurchaseController < ApplicationController
 private
   def set_card
     @card = Card.find_by(user_id: current_user)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
